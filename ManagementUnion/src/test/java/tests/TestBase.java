ddd7f2744746d40b0e7325c96a8579d1421c48ff
Anas Elbaioomy <111653305+13-ctrl@users.noreply.github.com>
2025-11-21 19:18:16 +0200

    Update TestBase.java

--- ManagementUnion/src/test/java/tests/TestBase.java
+++ ManagementUnion/src/test/java/tests/TestBase.java
@@ -1,5 +1,6 @@
 package tests;
 
+import Pages.LoginPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
@@ -8,11 +9,10 @@ import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
     protected WebDriver driver;
-
+    protected LoginPage loginPage;
     @BeforeMethod
     public void setUp() {
-        System.setProperty("webdriver.edge.driver",
-                "C:\\Users\\anase\\Desktop\\ManagementUnion\\drivers\\msedgedriver.exe");
+      System.setProperty("webdriver.edge.driver", "drivers/msedgedriver.exe");
 
         driver = new EdgeDriver();
         driver.manage().window().maximize();
